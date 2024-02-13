6d116f73e91db70962c4bd69c62f22173021fe4c
esyaahmad <esya.ahmad@gmail.com>
2024-02-13 17:28:52 +0700

    cors setup

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const app = express()
 const port = 3000
 const router = require('./routers/index')
 const errorHandler = require("./middlewares/errorHandler")
+const cors = require('cors');
 // const {uploader, cloudinaryConfig} = require ('./utils/cloudinaryConfig')
 
 
 //bodyparser
+app.use(cors());
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(router)
 app.use(errorHandler)
+
 // app.use(cloudinaryConfig)
 
 app.listen(port, () => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -57,6 +57,7 @@ class userController {
     }
 
 
+    
 }
 
 module.exports = userController
