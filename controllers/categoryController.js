2b0561ee2b09d45682a081e3b902f49b8ecbb22c
esyaahmad <esya.ahmad@gmail.com>
2024-02-13 12:10:43 +0700

    authentication done

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,18 +1,25 @@
+const { Category } = require('../models/index')
 
 
 class categoryController {
     static async addCategory(req,res,next) {
         try {
-            
+            // console.log(req.body);
+            const { name } = req.body
+            const category = await Category.create({ name })
+            res.status(201).json(category)
         } catch (error) {
-            console.log(error);
+           next(error)
         }
     }
+
     static async getCategories(req,res,next) {
         try {
-            
+            const category = await Category.findAll()
+            res.status(200).json({category})
         } catch (error) {
             console.log(error);
+           next(error)
         }
     }
 
